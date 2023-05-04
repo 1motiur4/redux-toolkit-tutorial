0c76f7bc8d61c9bce23d1d1aaed8b65ce5936142
Motiur Muhith <1motiur4@gmail.com>
2023-05-04 16:37:57 -0400

    513 done

--- vite-starter/src/features/cart/cartSlice.js
+++ vite-starter/src/features/cart/cartSlice.js
@@ -3,7 +3,7 @@ import cartItems from "../../cartItems";
 
 const initialState = {
   cartItems: cartItems,
-  amount: 0,
+  amount: 4,
   total: 0,
   isLoading: true,
 };
@@ -11,8 +11,25 @@ const initialState = {
 const cartSlice = createSlice({
   name: "cart",
   initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => {
+        return item.id !== itemId;
+      });
+    },
+    toggleAmount: (state, action) => {
+      const { id, inc } = action.payload;
+      console.log(action.payload);
+      const cartItem = state.cartItems.find((item) => item.id === id);
+      inc ? (cartItem.amount += 1) : (cartItem.amount -= 1);
+    },
+  },
 });
 
-// console.log(cartSlice)
+export const { clearCart, removeItem, toggleAmount } = cartSlice.actions;
 
 export default cartSlice.reducer;
